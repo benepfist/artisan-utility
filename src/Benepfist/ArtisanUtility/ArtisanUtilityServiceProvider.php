13de12910473242b75ab1a9cffa2a6bcb3ce7954
Benedict Pfisterer <pfisterer.bene@gmail.com>
2014-01-06 14:52:34 +0100

    Add ArtisanUtilityServiceProvider

---
+++ src/Benepfist/ArtisanUtility/ArtisanUtilityServiceProvider.php
@@ -0,0 +1,55 @@
+<?php namespace Benepfist\ArtisanUtility;
+
+use Illuminate\Support\ServiceProvider;
+
+class ArtisanUtilityServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('benepfist/artisan-utility');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->registerArtisanUtilityCommand();
+
+		$this->commands(
+			'laravel.add');
+	}
+
+	protected function registerArtisanUtilityCommand()
+	{
+		$this->app['laravel.add'] = $this->app->share(function($app){
+			$PackageInstaller = new PackageInstaller($app['files'], $app['config']);
+			return new ArtisanUtilityCommand($PackageInstaller);
+		});
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
