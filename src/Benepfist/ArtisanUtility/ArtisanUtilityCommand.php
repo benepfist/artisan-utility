ee881980297d155e17f2f28f701878921d88815d
Benedict Pfisterer <pfisterer.bene@gmail.com>
2014-01-09 18:30:39 +0100

    update ArtisanUtilityCommand

--- src/Benepfist/ArtisanUtility/ArtisanUtilityCommand.php
+++ src/Benepfist/ArtisanUtility/ArtisanUtilityCommand.php
@@ -89,8 +89,8 @@ class ArtisanUtilityCommand extends Command {
     protected function getOptions()
     {
         return array(
-			array('alias', null, InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, 'add to require-dev', null),
-			array('provider', null, InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, 'add to require-dev', null),			
+			array('alias', null, InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, 'add alias to app.aliases', null),
+			array('provider', null, InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, 'add provider to app.providers', null),			
 		);
     }	
 
